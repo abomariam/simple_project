113b195d2e4d05467a6bb865000c24b7acf3123f
Mahmoud Ahmed <eng.mahmoud.a.mostafa@gmail.com>
2020-04-27 16:01:41 +0200

    Arrange ReactJS code a little.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,96 +2,69 @@ import React, {useState} from 'react';
 import axios from 'axios';
 import './App.css';
 
-// function handleSubmit(event) {
-//   const start_point_lat = document.querySelector('#start-point-lat-input').value;
-//   const start_point_lng = document.querySelector('#start-point-lng-input').value;
-//   const end_point_lat = document.querySelector('#end-point-lat-input').value;
-//   const end_point_lng = document.querySelector('#end-point-lng-input').value;
-//
-//     axios
-//         .post('api/v1/calculate-distance/', {
-//                 start_point_lat: start_point_lat,
-//                 start_point_lng: start_point_lng,
-//                 end_point_lat: end_point_lat,
-//                 end_point_lng: end_point_lng
-//             }
-//         ).then(({data}) => {
-//         document.querySelector('#distance').textContent = `${data.distance}`
-//     })
-//         .catch(err => console.log(err))
-// }
+function PointInput({title, point, changePoint}) {
+    return (
+        <div className="point-container">
+            <fieldset>
+                <legend>{title}</legend>
+                <div className="input-container">
+                    <label htmlFor='start-point-lat-input'>Latitude:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
+                    <input id='start-point-lat-input' type='text'
+                           value={point.x}
+                           onChange={(event) => {
+                               changePoint({x: event.target.value, y: point.y})
+                           }}
+                    />
+                </div>
+                <div className="input-container">
+                    <label htmlFor='start-point-lng-input'>Longitude:&nbsp;&nbsp;</label>
+                    <input id='start-point-lng-input' type='text'
+                           value={point.y}
+                           onChange={(event) => {
+                               changePoint({x: point.x, y: event.target.value})
+                           }}
+                    />
+                </div>
+            </fieldset>
+        </div>
+    )
+}
 
 function App() {
-    const [startPointLat, setStartPointLat] = useState('');
-    const [startPointLng, setStartPointLng] = useState('');
-    const [endPointLat, setEndPointLat] = useState('');
-    const [endPointLng, setEndPointLng] = useState('');
+    const [startPoint, setStartPoint] = useState({x: '', y: ''});
+    const [endPoint, setEndPoint] = useState({x: '', y: ''});
+
     const [distance, setDistance] = useState('');
 
     const handleSubmit = (event) => {
         event.preventDefault();
+        console.log(startPoint);
         axios
-        .post('api/v1/calculate-distance/', {
-                start_point_lat: startPointLat,
-                start_point_lng: startPointLng,
-                end_point_lat: endPointLat,
-                end_point_lng: endPointLng
-            }
-        ).then(({data}) => {
+            .post('api/v1/calculate-distance/', {
+                    start_point_lat: startPoint.x,
+                    start_point_lng: startPoint.y,
+                    end_point_lat: endPoint.x,
+                    end_point_lng: endPoint.y
+                }
+            ).then(({data}) => {
             setDistance(data.distance);
         })
             .catch(err => console.log(err))
     }
-  return (
-    <div className="App">
-        <h1>Distance Calculator</h1>
-      <div>
-          <div className="point-container">
-          <fieldset>
-              <legend>Start Point</legend>
-              <div className="input-container">
-            <label htmlFor='start-point-lat-input'>Latitude:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-            <input id='start-point-lat-input' type='text'
-                   value={startPointLat}
-                   onChange={(event)=>{setStartPointLat(event.target.value)}}
-            />
-              </div>
-              <div className="input-container">
-            <label htmlFor='start-point-lng-input'>Longitude:&nbsp;&nbsp;</label>
-            <input id='start-point-lng-input' type='text'
-                   value={startPointLng}
-                   onChange={(event)=>{setStartPointLng(event.target.value)}}
-            />
-              </div>
-        </fieldset>
-          </div>
-          <div className="point-container">
-          <fieldset>
-              <legend>End Point</legend>
-              <div className="input-container">
-            <label htmlFor='end-point-lat-input'>Latitude:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</label>
-            <input id='end-point-lat-input' type='text'
-                    value={endPointLat}
-                   onChange={(event)=>{setEndPointLat(event.target.value)}}
-            />
-              </div>
-              <div className="input-container">
-            <label htmlFor='end-point-lng-input'>Longitude:&nbsp;&nbsp;</label>
-            <input id='end-point-lng-input' type='text'
-                    value={endPointLng}
-                   onChange={(event)=>{setEndPointLng(event.target.value)}}
-            />
-              </div>
-          </fieldset>
-          </div>
-        <button onClick={handleSubmit}>have?</button>
-      </div>
+    return (
+        <div className="App">
+            <h1>Distance Calculator</h1>
+            <div>
+                <PointInput title="Start Point" point={startPoint} changePoint={setStartPoint}/>
+                <PointInput title="End Point" point={endPoint} changePoint={setEndPoint}/>
+                <button onClick={handleSubmit}>have?</button>
+            </div>
 
-      <div>
-        <h3>{distance}</h3>
-      </div>
-    </div>
-  );
+            <div>
+                <h3>{distance}</h3>
+            </div>
+        </div>
+    );
 }
 
 export default App;
